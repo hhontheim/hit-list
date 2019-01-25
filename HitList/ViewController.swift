9520ce7a86cc6a546bf1a7bac4e8ae9dca810334
Henning Hontheim <henning@hontheim.net>
2019-01-25 20:16:14 +0100

    Initial Commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Henning Hontheim on 25.01.19.
+//  Copyright © 2019 Henning Hontheim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
